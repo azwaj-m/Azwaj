8e9844d4fe99d7c42ef373ae713fe9880ce536c3
Azwaj <AzwajMarriage@gmail.com>
2026-04-26 12:16:12 +0500

    Fixed firebase import and build issues

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
@@ -16,6 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider(); // گوگل لاگ ان کے لیے
-export const db = getFirestore(app); // ریئل ٹائم چیٹ کے لیے
-export const storage = getStorage(app); // تصاویر اپلوڈ کرنے کے لیے
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
